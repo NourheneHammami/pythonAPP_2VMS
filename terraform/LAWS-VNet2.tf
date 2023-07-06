0a1ba34eab9656ae4b5f68c890b91cd005b13951
NOURHENE HAMMAMI <nourhene.hammami@atos.net>
2023-07-06 14:34:42 +0200

    first version of infrastructure

---
+++ terraform/LAWS-VNet2.tf
@@ -0,0 +1,12 @@
+
+#creation de log_analytics_workspace #VNet2
+
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "acctest-01"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+#terraform import azurerm_log_analytics_workspace.workspace1 /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/mygroup1/providers/Microsoft.OperationalInsights/workspaces/workspace1
